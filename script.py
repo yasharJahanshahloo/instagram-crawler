3803d91f1479a9280085a107ec6c72a791bf5a20
yasharJahanshahloo <yashar.jahanshahloo@gmail.com>
2020-01-04 20:40:07 +0330

    script bug fixed

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 from elasticsearch_dsl import connections
 
-from inscrawler.elastic import insert_popular
+from inscrawler.elastic import insert_popular,Popular
 
 connections.create_connection(hosts=['localhost'], timeout=20)
+Popular.init()
 insert_popular(username="amir_mahdi_jule", followers=2500000, checked=False)
 
